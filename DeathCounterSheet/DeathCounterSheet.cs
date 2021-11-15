21b7d049bbc577810f2077169ccee9a4fc048d1e
TangentDay <stick.sugar.syndrome@gmail.com>
2021-11-16 03:17:32 +0900

    hookでファイル作成してみる

--- DeathCounterSheet/DeathCounterSheet.cs
+++ DeathCounterSheet/DeathCounterSheet.cs
@@ -9,14 +9,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Celeste.Mod.Example
 {
     public class DeathCounterSheet : EverestModule
     {
-
-        // Only one alive module instance can exist at any given time.
         public static DeathCounterSheet Instance;
+
+        public static readonly string DeathInfoDir = Path.Combine("Saves", "DeathInfo");
+        public static string DeathInfoSlotDir => Path.Combine(DeathInfoDir, SaveData.Instance?.FileSlot.ToString() ?? "-1");
+
 
         public DeathCounterSheet()
         {
@@ -30,6 +33,7 @@ namespace Celeste.Mod.Example
         // Load runs before Celeste itself has initialized properly.
         public override void Load()
         {
+            Everest.Events.Player.OnDie += PlayerOnDie;
         }
 
         // Optional, initialize anything after Celeste has initialized itself properly.
@@ -45,6 +49,19 @@ namespace Celeste.Mod.Example
         // Unload the entirety of your mod's content. Free up any native resources.
         public override void Unload()
         {
+            Everest.Events.Player.OnDie -= PlayerOnDie;
+        }
+
+        private void PlayerOnDie(Player player)
+        {
+            if (!Settings.Enabled) return;
+
+            string filePath = Path.Combine(DeathInfoSlotDir, $"{DateTime.Now.Ticks}.txt");
+            if (!Directory.Exists(DeathInfoSlotDir))
+            {
+                Directory.CreateDirectory(DeathInfoSlotDir);
+            }
+            File.Create(filePath).Close();
         }
     }
 }
